167cfd80acbf770e440b8f8b8d7eeddde9b2d21a
Gabriele D'Arrigo <darrigo.g@gmail.com>
2022-08-05 10:34:45 +0200

    Define database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { StreamsModule } from './streams/streams.module';
 import { LogisticProvidersModule } from './logistic-providers/logistic-providers.module';
+import database from './config/database';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://user:password@localhost:27017/wasteStreams?authSource=admin'),
+    ConfigModule.forRoot({
+      load: [database],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('mongodb.uri'),
+      })
+    }),
     StreamsModule,
     LogisticProvidersModule,
   ],
---
+++ src/config/database.ts
@@ -0,0 +1,5 @@
+export default () => ({
+  mongodb: {
+    uri: process.env.MONGODB_URI || 'mongodb://user:password@localhost:27017/wasteStreams?authSource=admin',
+  }
+});
